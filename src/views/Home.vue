abf7242cfc1aed32f569219aa597a15afb2a8b3f
bastienBernou <bastienbernou60@gmail.com>
2022-02-25 14:03:55 +0100

    footer D+M

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="home">
+    <FooterLayout/>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to O'Japon"/>
+
     <h1>On a plus qu'à bosser les copains :D</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import FooterLayout from '@/components/FooterLayoutMobile.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {    
+    FooterLayout,
+    
+
   }
 }
 </script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <FooterLayout />
+    <FooterLayoutMobile />
+  
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FooterLayout from '@/components/FooterLayout.vue';
+import FooterLayoutMobile from '@/components/FooterLayoutMobile.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    FooterLayout,
+    FooterLayoutMobile,
+    
+
+
+  }
+}
+</script>
