5cc16b9b95c1ee36dfb0bbafa1597e9910a56a1d
valgroux <valentin.groux.tic@gmail.com>
2022-02-25 11:37:43 +0100

    Feature search bar responsive à confirmer

---
+++ src/views/Composant.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Mes composants</h1>
+  <InputSearch />
+</template>
+
+<script>
+import InputSearch from '@/components/formulaire/InputSearch.vue'
+export default {
+  name: 'Composant',
+  components: {
+    InputSearch
+  }
+}
+</script>
+
+<style>
+
+</style>
