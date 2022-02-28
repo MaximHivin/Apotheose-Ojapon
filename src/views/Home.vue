623ed47755dc7ecf49bb6881da94e07bf574a54e
MaximHivin <hivin.maxim@hotmail.fr>
2022-02-28 11:34:27 +0100

    conflict resolution in progress

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to O'Japon"/>
-    <Card />
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import Card from '@/components/Card.vue'
+
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
-    Card,
   }
 }
 </script>
