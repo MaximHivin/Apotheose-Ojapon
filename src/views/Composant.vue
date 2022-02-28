4721e90a9a8784ebc52698cb8c552227c891cecf
MaximHivin <hivin.maxim@hotmail.fr>
2022-02-28 11:39:31 +0100

    conflict with Home.vue and Button components resolving

--- src/views/Composant.vue
+++ src/views/Composant.vue
@@ -1,14 +1,20 @@
 <template>
   <h1>Mes composants</h1>
   <InputText />
+  <Button btnName="se connecter" /><br><br>
+  <Button btnName="s'inscrire" /><br><br>
 </template>
 
 <script>
+
 import InputText from '@/components/formulaire/InputText.vue'
+import Button from '@/components/Button.vue'
+
 export default {
   name: 'Composant',
   components: {
-    InputText
+    InputText,
+    Button
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to O'Japon"/>
-    <Button btnName="se connecter" /><br><br>
-    <Button btnName="s'inscrire" /><br><br>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import Button from '@/components/Button.vue'
+
 
 export default {
   name: 'Home',
