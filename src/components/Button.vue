15c46c4a8f7574258f1333eec581f226bae822ef
MaximHivin <hivin.maxim@hotmail.fr>
2022-02-28 11:24:02 +0100

    conflict with Button components ended

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -18,40 +18,30 @@ export default {
 
 <style>
 
-    .btn-components {
+    .btn-connection {
        width: 196px;
        height: 54px;
        line-height: 54px;
        background-color: #CE1137;
        padding: 16px 24px;
        border-radius: 10px;   
+       font-weight: bold;
+      font-size: 18px;
+      text-decoration: none;
+      color: #fff;
     }
 
-    .btn-components:hover {
+    .btn-connection:hover {
         background-color: #fff;
         border: 1px solid #CE1137;
         color: #CE1137;
         transition: 400ms ease-in-out;
     }
 
-    .btn-connection {
-      font-weight: bold;
-      font-size: 18px;
-      text-decoration: none;
-      color: #fff;
-    }
-
     
 
     @media screen and (max-width: 375px) {
 
-      /* .btn-components {
-        width: 202px;
-      }
-
-      .btn-connection {
-          font-size: 18px;  
-      } */
     }
 
 </style>
