cb852c2bb6d42f556220820d3e12f8d41e7fb51e
valgroux <valentin.groux.tic@gmail.com>
2022-02-28 22:17:21 +0100

    Fixed feature button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,21 @@
 <template>
-
-
   <div class="home">
-    <HeaderLayout/>
-    <HeaderLayoutMobile/>
-    <FooterLayout />
-    <FooterLayoutMobile />
+
+    <Button btnName="Se connecter" />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Button from '@/components/Button.vue'
 
-import HeaderLayout from '@/components/HeaderLayout.vue';
-import HeaderLayoutMobile from '@/components/HeaderLayoutMobile.vue';
-import FooterLayout from '@/components/FooterLayout.vue';
-import FooterLayoutMobile from '@/components/FooterLayoutMobile.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    FooterLayout,
-    FooterLayoutMobile,
-    HeaderLayout,
-    HeaderLayoutMobile
+    Button
   }
 }
 </script>
