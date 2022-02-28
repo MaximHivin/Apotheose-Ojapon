3b8a4b6e50b8a501535f2add1b16d82f8862c25a
bastienBernou <bastienbernou60@gmail.com>
2022-02-28 11:26:54 +0100

    inputs

--- src/components/FormulaireDeConnexion.vue
+++ src/components/FormulaireDeConnexion.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>test</h1>
+    <div class="main_container">
+        
+        <h1 class="Title">Connectez-vous à votre compte</h1>
+        <div class="inputs">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Mot de passe">
+        </div>
+        
+
+        
+    </div> 
+    
+
 </template>
 
 <script>
@@ -15,5 +27,9 @@ export default {
 </script>
 
 <style scoped>
+.main_container {
+     max-width : 1642px;
+     margin: 0 auto;
+}
 
 </style>
