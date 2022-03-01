cc62a78da0022943cb9714c1624baa964e3abf64
MaximHivin <hivin.maxim@hotmail.fr>
2022-03-01 12:11:42 +0100

    new view backoffice in progress

---
+++ src/views/BackOffice.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <HeaderLayout />
+      
+      <CreationGuide LinkContent="Créer un guide voyage" />
+      <CreationGuide LinkContent="Créer un guide voyage" />
+      <CreationGuide LinkContent="Créer un guide voyage" />
+      <CreationGuide LinkContent="Créer un guide voyage" />
+
+      <FooterLayout />
+      <FooterLayoutMobile />
+  </div>
+</template>
+
+<script>
+
+import HeaderLayout from '@/components/HeaderLayout.vue'
+import FooterLayout from '@/components/FooterLayout.vue'
+import FooterLayoutMobile from '@/components/FooterLayoutMobile.vue'
+
+import CreationGuide from '@/components/CTA/CreationGuide.vue'
+
+export default {
+    name: 'BackOffice',
+    components: {
+        HeaderLayout,
+        CreationGuide,
+        FooterLayout,
+        FooterLayoutMobile
+
+    }
+
+}
+
+</script>
+
+
+<style>
+
+
+
+</style>
