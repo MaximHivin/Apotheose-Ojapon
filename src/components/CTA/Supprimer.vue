27472eb78be008850f560f38f1ec86da2ed141c3
valgroux <valentin.groux.tic@gmail.com>
2022-03-01 14:48:11 +0100

    Ajustment v2 component ajouter

--- src/components/CTA/Supprimer.vue
+++ src/components/CTA/Supprimer.vue
@@ -1,43 +1,54 @@
 <template>
-  <div class="delete-element">
-      <img alt="trash icon" src="@/assets/trash.svg">
+<a href="#" class="delete">
+  <div class="delete__element">
+      <img alt="delete element" src="@/assets/trash.svg">
   </div>
+  </a>
 </template>
 
 <script>
 export default {
-    name: 'Supprimer',
+    name: 'Ajouter',
 
 }
 </script>
 
 <style>
+@import url('../../assets/css/style.css');
 
-    .delete-element {
-        height: 50px;
-        width: 50px;
+    .delete{
+        display:inline-block;
+    }
+
+    .delete__element {
+        height: 5em;
+        width: 5em;
         border-radius: 50%;
-        background-color: #CE1137;
+        background-color: var(--primary-color);
         display: flex;
         justify-content: center;
         align-items: center;
-        
+        transition-property: margin-top;
+        transition-duration: 0.2s;
+        transition-delay: 0.5ms;
+    }
+
+    .delete__element:hover{
+        margin-top: 1.2em;
     }
 
     img {
-        height: 22px;
+        height: 2.2em;
     }
 
     @media screen and (max-width: 375px) {
-
-        .delete-element {
-            height: 40px;
-            width: 40px;
+        
+        .delete__element {
+            height: 4em;
+            width: 4em;
         }
         
 
     }
-
-
 
 </style>
