461a7574bbd09154fe1a3f50f4255e1c32c4a738
valgroux <valentin.groux.tic@gmail.com>
2022-03-01 14:03:27 +0100

    En cours

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,28 +2,19 @@
 
 
   <div class="home">
-    <HeaderLayout/>
-    <HeaderLayoutMobile/>
-    <FooterLayout />
-    <FooterLayoutMobile />
+    <Retour />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import HeaderLayout from '@/components/HeaderLayout.vue';
-import HeaderLayoutMobile from '@/components/HeaderLayoutMobile.vue';
-import FooterLayout from '@/components/FooterLayout.vue';
-import FooterLayoutMobile from '@/components/FooterLayoutMobile.vue'
+import Retour from '@/components/CTA/Retour.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    FooterLayout,
-    FooterLayoutMobile,
-    HeaderLayout,
-    HeaderLayoutMobile
+    Retour
   }
 }
 </script>
