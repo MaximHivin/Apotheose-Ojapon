593ca396f606288701f4d96d27e5a5b858a899d5
valgroux <valentin.groux.tic@gmail.com>
2022-03-01 11:59:07 +0100

    Component Ajouter fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,28 +2,19 @@
 
 
   <div class="home">
-    <HeaderLayout/>
-    <HeaderLayoutMobile/>
-    <FooterLayout />
-    <FooterLayoutMobile />
+    <Ajouter/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import HeaderLayout from '@/components/HeaderLayout.vue';
-import HeaderLayoutMobile from '@/components/HeaderLayoutMobile.vue';
-import FooterLayout from '@/components/FooterLayout.vue';
-import FooterLayoutMobile from '@/components/FooterLayoutMobile.vue'
+import Ajouter from '@/components/CTA/Ajouter.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    FooterLayout,
-    FooterLayoutMobile,
-    HeaderLayout,
-    HeaderLayoutMobile
+    Ajouter
   }
 }
 </script>
