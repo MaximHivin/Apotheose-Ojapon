4f4eb4fe1150ed807f38ded3c9bc3d11d55ecf06
valgroux <valentin.groux.tic@gmail.com>
2022-03-02 10:37:12 +0100

    Delete container and ajustment for component InputSearch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,18 +10,20 @@
     <InputText />
     <Tag TagContent="Nom du tag"/>
     <TextArea />
+    <InputSearch />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import InputSearch from '@/components/formulaire/InputSearch.vue'
 // import TextArea from '@/components/formulaire/TextArea.vue'
 // import Tag from '@/components/formulaire/Tag.vue'
 // import InputText from '@/components/formulaire/InputText.vue'
 // import Supprimer from '@/components/CTA/Supprimer.vue';
 // import Retour from '@/components/CTA/Retour.vue';
-import ButtonBack from '@/components/CTA/ButtonBack.vue';
+// import ButtonBack from '@/components/CTA/ButtonBack.vue';
 // import Button from '@/components/Button.vue'
 // import CirclePi from '@/components/CirclePi.vue';
 // import Card from '@/components/Card.vue';
@@ -33,7 +35,7 @@ export default {
 
   // Supprimer
   //   Retour,
-    ButtonBack
+    // ButtonBack
   //   Card,
   //   Ajouter,
   //   CirclePi,
@@ -41,6 +43,7 @@ export default {
   // InputText
   // Tag
   // TextArea
+  InputSearch
   }
 }
 </script>
