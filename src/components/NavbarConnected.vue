7b81db53f6532d0299637e66033fe26eb8322b2c
valgroux <valentin.groux.tic@gmail.com>
2022-03-04 14:33:42 +0100

    Creation component NavbarConnected + dynamisation page home / login / poi

---
+++ src/components/NavbarConnected.vue
@@ -0,0 +1,116 @@
+<template>
+
+<div class="main_container">
+      <nav class="header">
+      <div class="header-left"><img src="../assets/logo.png" alt="logo"></div>
+      <div class="header__center">
+          <ul>
+              <li class="header__center-item"><router-link v-for="link in links" :key="link.name" :to="link.route">{{ link.name }}</router-link></li>
+
+          </ul>
+      </div>
+      <div class="header__right">
+          <ul>
+              <li class="header__right-inscription"><a href="#">Se deconnecter</a></li>
+              <li><Button btnName="Accès à mon back office"/></li>
+          </ul>
+      </div>
+  </nav>
+</div>
+
+  
+</template>
+
+<script>
+import Button from '@/components/Button.vue'
+export default {
+    name:'NavbarConnected',
+    components: {
+        Button
+    },
+    data () {
+            return {
+                links: [
+                    {
+                        name: 'Accueil',
+                        route: '/'
+                    },
+                    {
+                        name: 'LeJapon',
+                        route: '/lejapon'
+                    },
+                    {
+                        name: 'PointsInteret',
+                        route: '/pointsinteret'
+                    },
+                    {
+                        name: 'BlogsVoyageurs',
+                        route: '/blogs'
+                    },
+                    {
+                        name: 'FAQ',
+                        route: '/faq'
+                    },
+                    {
+                        name: 'Contact',
+                        route: '/contact'
+                    }
+                    
+                ]
+            }
+        }
+
+}
+</script>
+
+<style scoped>
+@import url('../assets/css/style.css');
+
+.header{
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+}
+
+
+.header__center{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.header__center ul{
+    list-style: none;
+}
+
+.header__center-item a{
+    font-size: 1.6em;
+    font-family: 'Fellix SemiBold';
+    color: #fff;
+    text-decoration: none;
+    margin-left: 2.4em;
+}
+
+.header__right ul{
+    display:flex;
+    align-items: center;
+    white-space: nowrap;
+    list-style: none;
+}
+
+.header__right-inscription a{
+    text-decoration: none;
+    color:#fff;
+    font-size: 1.6em;
+    font-family: 'Fellix Bold';
+    margin-right: 3.2em;
+}
+
+.header__right-inscription a:hover{
+    color:var(--primary-color);
+    transition: 0.5s;
+}
+
+</style>
