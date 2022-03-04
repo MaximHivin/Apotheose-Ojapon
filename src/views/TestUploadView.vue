3b7d62d8f632d1b0d74f08cde4ff4154931fbd35
AurelieCuignet <aurelie.cuignet@gmail.com>
2022-03-04 11:55:40 +0100

    event ok

--- src/views/TestUploadView.vue
+++ src/views/TestUploadView.vue
@@ -2,8 +2,8 @@
   <div>
       <h1>Test upload API</h1>
       <fieldset>
-            <label class="email_mdp" for="username">Choisir un fichier :</label>
-            <InputText name="file" id="file" placeholder="choose a file..." type="file" @inputChange="updateInputValue" />
+            <label for="file">Choisir un fichier :</label>
+            <input @change="upload($event)" name="file" id="file" placeholder="choose a file..." type="file" class="container__inputText-content">
         </fieldset>
         <div class="center-button">
             <Button v-on:click="uploadImg" btnName="Télécharger"/>
@@ -12,18 +12,54 @@
 </template>
 
 <script>
-import InputText from '@/components/formulaire/InputText.vue';
 import Button from '@/components/Button.vue';
 
 export default {
     name: 'TestUploadView', 
     components: {
-        InputText,
         Button
+    },
+    data () {
+        return {
+            errors: [],
+            success: null,
+            formData: {
+                password: '',
+                username: ''
+            }
+        }
+    },
+    methods: {
+        upload: function (event) {
+            console.log('method upload file input');
+            console.log(event);
+            //this.formData[value.name] = value.value
+        },
+        uploadImg() {
+            
+        }
     }
 }
 </script>
 
 <style>
+.container__inputText-content{
+    padding-left: 2.4em;
+    padding-top: 1.6em;
+    padding-bottom: 1.6em; 
+    border-radius: 5px;
+    border:none;
+    background-color:#F1F1F1 ;
+    color: var(--secondary-color);
+    font-size: 1.6em;
+    font-family: 'Fellix Medium';
+    font-weight: 500;
+    width: 100%;
+    box-sizing: border-box; 
+}
 
+.container__inputText-content::placeholder{
+    color:var(--regular-black);
+    font-weight: 500;
+}
 </style>
