58a927c542060b0e527e40738a46136b255895d6
AurelieCuignet <aurelie.cuignet@gmail.com>
2022-03-04 11:32:12 +0100

    structure ok

--- src/views/TestUploadView.vue
+++ src/views/TestUploadView.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
       <h1>Test upload API</h1>
+      <fieldset>
+            <label class="email_mdp" for="username">Choisir un fichier :</label>
+            <InputText name="file" id="file" placeholder="choose a file..." type="file" @inputChange="updateInputValue" />
+        </fieldset>
+        <div class="center-button">
+            <Button v-on:click="uploadImg" btnName="Télécharger"/>
+        </div>
   </div>
 </template>
 
 <script>
+import InputText from '@/components/formulaire/InputText.vue';
+import Button from '@/components/Button.vue';
+
 export default {
-    name: 'TestUploadView'
+    name: 'TestUploadView', 
+    components: {
+        InputText,
+        Button
+    }
 }
 </script>
 
