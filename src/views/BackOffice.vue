911225db6fd1d4da48e610ba435539524711c2bf
valgroux <valentin.groux.tic@gmail.com>
2022-03-07 10:40:11 +0100

    Add menu dynamic on BackOffice view

--- src/views/BackOffice.vue
+++ src/views/BackOffice.vue
@@ -1,13 +1,14 @@
 <template>
 
  <div class="main_container">
-    <HeaderLayout />
+    <NavbarConnected v-if="this.$store.state.token"/>
+    <HeaderLayout v-else />
 </div>
 
 <section class="backoffice">
     <div class="main_container">
         <div class="backoffice-title">
-            <h2>Bonjour <span>A DYNAMISER </span></h2>
+            <h2>Bonjour <span>{{ username }}</span></h2>
             <h3>Vous souhaitez :</h3>
         </div>
         <div class="backoffice__card">
@@ -33,17 +34,20 @@
 <script>
 
 import HeaderLayout from '@/components/HeaderLayout.vue'
+import NavbarConnected from '@/components/NavbarConnected.vue'
 import ButtonBack from '@/components/CTA/ButtonBack.vue'
 
 export default {
     name: 'BackOffice',
     components: {
         HeaderLayout,
+        NavbarConnected,
         ButtonBack
         // FooterLayoutMobile
-
+    },
+    props: {
+        username: String
     }
-
 }
 
 </script>
